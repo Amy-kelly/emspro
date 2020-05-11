801b2fe18804319d1c177b0746d926ff3ca511cc
carrie <2153074275@qq.com>
2020-05-12 02:08:33 +0800

    新创建一个类

--- userapp/views.py
+++ userapp/views.py
@@ -2,10 +2,15 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+
+
 def index(request):
     return HttpResponse()
 
 def demo(request):
     print("我是分支,这次正常了")
-
     return HttpResponse("view完成")
+
+class UserView(View):
+    pass
